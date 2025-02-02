6fba55c67b4954b8dd3030384aef3de707903679
KaioAlexandreFernandes <kaio.fernandes@unifesp.br>
2025-02-02 18:24:53 -0300

    1010.c

---
+++ 1010.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int cod1, num1, cod2, num2;
+    float valor1, valor2;
+
+    scanf("%d %d %f", &cod1, &num1, &valor1);
+    scanf("%d %d %f", &cod2, &num2, &valor2);
+
+    float total = num1 * valor1 + num2 * valor2;
+    
+    printf("VALOR A PAGAR: R$ %.2f\n", total);
+
+    return 0;
+}
