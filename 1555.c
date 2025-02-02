66dabf825d2603d48721f1019580d7025ec6a89f
KaioAlexandreFernandes <kaio.fernandes@unifesp.br>
2025-02-02 12:47:16 -0300

    1555.c

---
+++ 1555.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int rafael(int x, int y) {
+    return (3 * x) * (3 * x) + y * y;
+}
+
+int beto(int x, int y) {
+    return 2 * (x * x) + (5 * y) * (5 * y);
+}
+
+int carlos(int x, int y) {
+    return -100 * x + y * y * y;
+}
+
+int main() {
+    int N, x, y;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &x, &y);
+        int r = rafael(x, y);
+        int b = beto(x, y);
+        int c = carlos(x, y);
+
+        if (r > b && r > c) {
+            printf("Rafael ganhou\n");
+        } else if (b > r && b > c) {
+            printf("Beto ganhou\n");
+        } else {
+            printf("Carlos ganhou\n");
+        }
+    }
+    return 0;
+}
